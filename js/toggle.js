b53afaaabb1a157f923384bab07b75a84fac4127
Arman Abovyan <armani.abo@gmail.com>
2020-01-08 16:02:31 +0400

    fix toggle issue

--- js/toggle.js
+++ js/toggle.js
@@ -1,59 +1,42 @@
-
 Vue.component('toggle-select', {
-  model: {
-    prop: 'checked',
-    event: 'change'
-  },
-  props:   [
-    "title",
-    "name",
-    "disabled",
-    "tooltip",
-    "checked",
-    "selecteddescription",
-    "unselecteddescription"
-  ],
+    model: {
+        prop: 'checked',
+        event: 'change'
+    },
+    props: [
+        'title',
+        'name',
+        'disabled',
+        'tooltip',
+        'checked',
+        'selecteddescription',
+        'unselecteddescription'
+    ],
+    data () {
+      return {
+        value: this.checked
+      }
+    },
     template: `
     <div data-toggle="tooltip" data-placement="top" :title=tooltip>
-
-               <div class="switch-wrapper" >
-               
-               <span>
-               <p class="toggle-title"> {{title}}
-               </p>
-
-                <p v-if="!!checked && !!selecteddescription" class="switch-description">
-                {{selecteddescription}}
-                </p>
-                <p v-if="!checked && !!unselecteddescription" class="switch-description">
-                {{unselecteddescription}}
-                </p>
-                
-               </span>
-
-                <label class="switch" >
-
-                    <input 
-                    :checked="checked" 
-                    :id="name"
-                    :name="name" 
-                    type="checkbox" 
-                    :disabled="disabled" 
-                    v-on:change="$emit('change', $event.target.checked)"
-                    >
+         <div class="switch-wrapper">
+            <span>
+                <p class="toggle-title">{{title}}</p>
+                <p v-if="!!value && !!selecteddescription" class="switch-description">{{selecteddescription}}</p>
+                <p v-if="!value && !!unselecteddescription" class="switch-description">{{unselecteddescription}}</p>
+            </span>
+            <label class="switch" >
+                <input :checked="checked" :id="name" :name="name" type="checkbox" :disabled="disabled"
+                    @change="$emit('change', $event.target.checked)" v-model="value">
                 <span class="slider"></span>
             </label>
-            </div>
-</div>`,
+         </div>
+    </div>`,
 
-
-   mounted() {
-    $(function () {
-      $('[data-toggle="tooltip"]').tooltip()
-      console.log("mounted with: " + this.value);
-    })
-   }
-
-
-
-  })
+    mounted () {
+        $(function () {
+            $('[data-toggle="tooltip"]').tooltip()
+            console.log('mounted with: ' + this.value)
+        })
+    }
+})
